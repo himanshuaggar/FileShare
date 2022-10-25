68d2c09d36b6e6de850d13a130df9d199f524552
himanshuaggar <himanshuaggar00@gmail.com>
2022-10-25 20:58:08 +0530

    url change

--- Frontend/index.js
+++ Frontend/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://fileshare-himu.netlify.app";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
