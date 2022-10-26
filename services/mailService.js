4caebc991f0794cd1800249be6e914a20574defd
himanshuaggar <himanshuaggar00@gmail.com>
2022-10-26 17:46:18 +0530

    css change

--- services/mailService.js
+++ services/mailService.js
@@ -1,32 +1,3 @@
-// const nodemailer = require('nodemailer');
-
-// async function sendMail({from, to , subject, text, html}){
-
-//     let transporter = nodemailer.createTransport({
-//         host:process.env.SMTP_HOST,
-//         port:process.env.SMTP_PORT,
-//         secure:false,
-//         auth: {
-//             user: process.env.MAIL_USER,
-//             pass: process.env.MAIL_PASS
-//         }
-//     });
-
-//     let info : await transporter.sendMail({
-//         from:`fileShare <${from}>`,
-//         to:to,
-//         subject:subject,
-//         text:text,
-//         html:html
-
-//     });
-    
-// }
-
-
-// module.exports = sendMail;
-
-
 const nodemailer = require("nodemailer");
 module.exports = async ({ from, to, subject, text, html}) => {
         let transporter = nodemailer.createTransport({
