42a496fa15370912fbf1a23ef672618eff7e8ead
himanshuaggar <himanshuaggar00@gmail.com>
2022-10-31 19:12:11 +0530

    changes in file address

--- config/db.js
+++ config/db.js
@@ -17,3 +17,19 @@ function connectDB(){
 }
 
 module.exports = connectDB;
+
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// function connectDB() {
+//     // Database connection 🥳
+//     mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+//     const connection = mongoose.connection;
+//     connection.once('open', () => {
+//         console.log('Database connected 🥳🥳🥳🥳');
+//     })
+//     // .catch(err => {
+//     //     console.log('Connection failed ☹️☹️☹️☹️');
+//     // });
+// }
+
+// module.exports = connectDB;
